e24d39909fa3a395ed0aee7977df0a68866cc2bb
Enias <dgonssailar34@gmail.com>
2023-10-16 22:11:51 +0300

    Spring Qualifier Dog

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -5,5 +5,8 @@ import org.springframework.stereotype.Component;
 
 @Component("dog")
 public class Dog extends Animal {
-
+    @Override
+    public String toString() {
+        return "Im a Dog";
+    }
 }
